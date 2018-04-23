a5227c7557450a8977bc2632134f6bd6952acd7c
cyjlnk <2743319061@qq.com>
2018-04-23 23:15:38 +0800

    4-1核心库增加butterKnife

---
+++ latte-core/src/main/java/com/diabin/latte/app/activities/ProxyActivity.java
@@ -0,0 +1,10 @@
+package com.diabin.latte.app.activities;
+
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * Created by Administrator on 2018/4/23 0023.
+ */
+
+public abstract class ProxyActivity extends SupportActivity {
+}
---
+++ latte-core/src/main/java/com/diabin/latte/app/delegates/BaseDelegate.java
@@ -0,0 +1,14 @@
+package com.diabin.latte.app.delegates;
+
+import me.yokeyword.fragmentation_swipeback.SwipeBackFragment;
+
+/**
+ * Created by Administrator on 2018/4/23 0023.
+ */
+
+public abstract class BaseDelegate extends SwipeBackFragment {
+
+    public abstract Object setLayout();
+
+
+}
